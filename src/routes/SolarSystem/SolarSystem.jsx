e70bacbbb07a478bbf35a6c41c8323fffe351948
asalamgit <alisalamm95120@gmail.com>
2023-04-23 18:37:32 +0200

    systeme solaire terre et mars qui tourne autour du soleil ajout des image

--- src/routes/SolarSystem/SolarSystem.jsx
+++ src/routes/SolarSystem/SolarSystem.jsx
@@ -1,19 +1,25 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import classes from './SolarSystem.module.scss';
+import sun from '../../assets/sun2.png';
+import planetEarth from '../../assets/planet-earth.svg';
+import planetMars from '../../assets/planet-mars.svg';
 
 const SolarSystem = () => {
 	return (
-		// <div className={classes.solar-system}>
-			<div className={classes.solarSystem}>
-      <div className={classes.sun}></div>
-      <div className={classes.earthOrbit}>
-        <div className={classes.earth}></div>
-      </div>
-      <div className={classes.marsOrbit}>
-        <div className={classes.mars}></div>
-      </div>
-    </div>
-		// </div>
+		<div className={classes.solarSystem}>
+			<img src={sun} className={classes.sun} alt="sun" />
+			<Link to="/earth">
+				<div className={classes.earthOrbit}>
+          <img src={planetEarth} className={classes.earth} alt="earth" />
+				</div>
+			</Link>
+			<Link to="/mars">
+				<div className={classes.marsOrbit}>
+          <img src={planetMars} className={classes.mars} alt="earth" />
+				</div>
+			</Link>
+		</div>
 	);
 };
 
