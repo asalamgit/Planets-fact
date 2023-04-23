3a794f4ad594d1d16784187fba575d0c1f7a2949
asalamgit <alisalamm95120@gmail.com>
2023-04-23 22:09:08 +0200

    system solaire en cours , moitie des planete fait

--- src/routes/SolarSystem/SolarSystem.jsx
+++ src/routes/SolarSystem/SolarSystem.jsx
@@ -2,21 +2,38 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import classes from './SolarSystem.module.scss';
 import sun from '../../assets/sun2.png';
+import planetMercury from '../../assets/planet-mercury.svg';
+import planetVenus from '../../assets/planet-venus.svg';
 import planetEarth from '../../assets/planet-earth.svg';
 import planetMars from '../../assets/planet-mars.svg';
+import planetJupiter from '../../assets/planet-jupiter.svg';
+import planetSaturn from '../../assets/planet-saturn.svg';
+import planetUranus from '../../assets/planet-uranus.svg';
+import planetNeptune from '../../assets/planet-neptune.svg';
+import clsx from 'clsx';
 
 const SolarSystem = () => {
 	return (
 		<div className={classes.solarSystem}>
 			<img src={sun} className={classes.sun} alt="sun" />
-			<Link to="/earth">
-				<div className={classes.earthOrbit}>
-          <img src={planetEarth} className={classes.earth} alt="earth" />
+			<Link to="/mercury">
+				<div className={clsx(classes.planetOrbit, classes.mercuryOrbit)}>
+					<img src={planetMercury} className={clsx(classes.planet, classes.mercury)} alt="mercury" />
+				</div>
+			</Link>
+			<Link to="/venus">
+				<div className={clsx(classes.planetOrbit, classes.venusOrbit)}>
+					<img src={planetVenus} className={clsx(classes.planet,classes.venus)} alt="venus" />
+				</div>
+			</Link>
+			 <Link to="/earth">
+				<div className={clsx(classes.planetOrbit, classes.earthOrbit)}>
+          <img src={planetEarth} className={clsx(classes.planet,classes.earth)} alt="earth" />
 				</div>
 			</Link>
 			<Link to="/mars">
-				<div className={classes.marsOrbit}>
-          <img src={planetMars} className={classes.mars} alt="earth" />
+				<div className={clsx(classes.planetOrbit,classes.marsOrbit)}>
+          <img src={planetMars} className={clsx(classes.planet,classes.mars)} alt="mars" />
 				</div>
 			</Link>
 		</div>
